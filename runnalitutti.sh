8fa836d73c5eb1e49e4c4a6a5b60834262330b17
dustech <stefano.cerruti@outlook.com>
2024-03-09 13:35:19 +0100

    script di run

---
+++ runnalitutti.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#\ ./S05\ -\ Mini-Project\ Challenges/01\ -\ Intro\ &\ Mini-Project\ Starter/mini-projects-starter/mini-projects-starter/index.html
+#\ ./S05\ -\ Mini-Project\ Challenges/02\ -\ Pricing\ Cards/pricing-cards/pricing-cards/index.html
+#\ ./S05\ -\ Mini-Project\ Challenges/03\ -\ Ratings\ Component/ratings-component/ratings-component/index.html
+#\ ./S05\ -\ Mini-Project\ Challenges/04\ -\ User\ List/user-list/user-list/index.html
+chromium-browser ./S07\ -\ Website\ 1\ -\ Ebook\ Website/01\ -\ Project\ Setup/ebook-website-final/ebook-website-final/index.html & \
+chromium-browser ./S08\ -\ Website\ 2\ -\ Corso\ Training\ Course\ Website/01\ -\ Project\ Setup/corso-website-final/corso-website-final/index.html & \
+chromium-browser ./S09\ -\ Website\ 3\ -\ Portfolio\ Website/01\ -\ Project\ Setup/portfolio-website-final/portfolio-website-final/index.html & \
+chromium-browser ./S10\ -\ Website\ 4\ -\ Yavin\ Office\ Design/01\ -\ Project\ Setup/yavin-website-final/yavin-website-final/index.html & \
+chromium-browser ./S11\ -\ Website\ 5\ -\ Vera\ Software\ Solutions/01\ -\ Project\ Setup/vera-website-final/vera-website-final/index.html &
